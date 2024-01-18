cdbeba846c3539fe1b574075bf16b296e421e46a
Álvaro Gabriel <131178055+alvaroggp@users.noreply.github.com>
2024-01-18 17:25:13 -0300

    refatorando codigo visando melhor legibilidade

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,114 +1,23 @@
-from utilitarios import formatacao
-
+from utilitarios import cabecalho ,formatacao ,escolha_simbolo, loop_principal
 
 continuar = True
-contador = 0
+
 
 # Cabeçalho do jogo
-print("|-----Jogo-da-velha-----|\n")
+cabecalho()
 
 while continuar:
     
     # Criando as posiçoes da matriz
     matriz = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
     
-    # Designando os simbolos personalizados
-    while True:
-            
-            simbolo_1 = str(input("Qual simbolo o Jogador 1 quer usar: "))
-            
-            if len(simbolo_1) > 1 :
-                while True:
-                    
-                    simbolo_1 = str(input("Infelizmente o Simbolo escolhido é invalido, por favor escolha outro: "))
-                    
-                    if len(simbolo_1) == 1:
-                        break
-
-            simbolo_2 = str(input("Qual simbolo o jogador 2 quer usar: "))
-
-            if len(simbolo_2) > 1 or simbolo_2 == simbolo_1:
-                while True:
-                    
-                    simbolo_2 = str(input("Infelizmente o Simbolo escolhido é invalido, por favor escolha outro: "))
-                    
-                    if len(simbolo_2) == 1 and simbolo_2 != simbolo_1:
-                        break
-
-            else:
-                break
+    # Designando os simbolos personalizados 
+    simbolo_1 = escolha_simbolo(1)
+    simbolo_2 = escolha_simbolo(2)
     
-    # Loop central do jogo
-    while True:
+    formatacao(matriz)
 
-        # Chamando a formatação da matriz
-        formatacao(matriz)
-        
-        # Codigo referente ao Jogador 1
-        jogador_1 = int(input("Qual posição o Jogador 1 deseja jogar: "))
-        
-        if matriz[jogador_1] == simbolo_2 or matriz[jogador_1] == simbolo_1 or 0 >jogador_1 > 8:
-
-            while True:
-                
-                jogador_1 = int(input("A resposta não é valida, tente novamente: "))
-                
-                if matriz[jogador_1] ==simbolo_2 or matriz[jogador_1] == simbolo_1:
-                    continue
-
-                else: 
-                    matriz[jogador_1] = simbolo_1
-                    break
-        else:
-        
-            matriz[jogador_1] = simbolo_1
-
-        formatacao(matriz)
-
-        if any(all(matriz[i] == simbolo_1 for i in sublista) for sublista in [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
-        [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]):
-
-            print("jogador 1 venceu!!!")
-
-            break
-        
-        contador += 1
-
-        if contador == 9:
-
-            print("Deu velha!!!")
-
-            break
-
-        # Codigo referente ao Jogador 2
-        jogador_2 = int(input("Qual posição o Jogador 2 deseja jogar: "))
-
-        if matriz[jogador_2] == simbolo_2 or matriz[jogador_2] == simbolo_1 or 0 > jogador_2 > 8:
-            
-            while True:
-
-                jogador_2 = int(input("A resposta não é valida, tente novamente: "))
-
-                if matriz[jogador_2] == simbolo_2 or matriz[jogador_2] == simbolo_1:
-
-                    continue
-
-                else: 
-
-                    matriz[jogador_2] = simbolo_2
-
-                    break
-    
-        matriz[jogador_2] = simbolo_2
-
-        if any(all(matriz[i] == simbolo_2 for i in sublista) for sublista in [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
-        [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]):
-
-            print("jogador 2 venceu!!!")
-
-            break
-
-        contador += 1
+    loop_principal(matriz,simbolo_1, simbolo_2)
     
     # Perguntando se o loop deve continuar
     pergunta = input("Quer jogar novamente (sim[1]/não[2]): ")
--- utilitarios.py
+++ utilitarios.py
@@ -1,3 +1,29 @@
+def cabecalho():
+    print("|-----------------------|")
+    print("|-----Jogo-da-velha-----|")
+    print("|-----------------------|")
+
+
+
+def escolha_simbolo(j):
+    if j == 1:
+        simbolo = input("Qual o simbolo que o jogador numero 1 deseja utilizar: ")
+    
+    else:
+        simbolo = input("Qual o simbolo que o jogador numero 2 deseja utilizar: ")
+
+        if len(simbolo) > 1 :
+                while True:
+                    
+                    simbolo_1 = str(input("Infelizmente o Simbolo escolhido é invalido, por favor escolha outro: "))
+                    
+                    if len(simbolo) == 1:
+                        break
+                    break
+    return simbolo
+
+
+
 def formatacao(matriz):
     print(f" {matriz[0]} | {matriz[1]} | {matriz[2]}")
     print("---|---|---")
@@ -5,3 +31,63 @@ def formatacao(matriz):
     print(("---|---|---"))
     print(f" {matriz[6]} | {matriz[7]} | {matriz[8]}")
     
+
+
+def jogadores(j, matriz, simbolo_x, simbolo_y):            
+    jogador = int(input(f"Qual posição o Jogador {j} deseja jogar: "))
+    
+    if matriz[jogador] == simbolo_y or matriz[jogador] == simbolo_x or 0 >jogador > 8:
+
+        while True:
+            
+            jogador = int(input("A resposta não é valida, tente novamente: "))
+              
+    else:
+    
+        matriz[jogador] = simbolo_x
+    
+    formatacao(matriz)
+
+    
+    
+
+def verificando_vitoria(j, matriz, simbolo_x):
+    if any(all(matriz[i] == simbolo_x for i in sublista) for sublista in [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
+    [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]):
+        
+        print(f"O jogador {j} venceu!!!")
+        return True
+
+    else:
+        return False 
+        
+
+
+def verificando_velha(contador):
+    print(contador)
+    if contador ==  8:
+        
+        print("DEU VELHA!!!")
+        return True
+
+
+
+def loop_principal(matriz, simbolo_1, simbolo_2):
+    contador = 0
+    while True:
+        jogadores(1, matriz, simbolo_1, simbolo_2)
+        venceu_1 = verificando_vitoria(1, matriz, simbolo_1)
+        if venceu_1: break
+        
+        velha = verificando_velha(contador)
+        contador += 1
+        if velha: break
+        
+        jogadores(2, matriz, simbolo_2, simbolo_1)
+        venceu_2 = verificando_vitoria(2, matriz, simbolo_2)
+        if venceu_2: break
+        
+        velha = verificando_velha(contador)
+        contador += 1
+        if velha: break
+        
